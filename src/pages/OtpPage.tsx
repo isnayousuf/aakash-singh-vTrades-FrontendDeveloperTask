82495e2e9e62101e98a54f6c81ca39366c1bc2b9
isna yousuf <isnayousuf77@gmail.com>
2025-04-18 17:30:07 +0530

    fix: use email filled in the message

--- src/pages/OtpPage.tsx
+++ src/pages/OtpPage.tsx
@@ -4,9 +4,10 @@ import Header from "../containers/Login/Header"
 import OtpComponent from "../containers/Login/OtpComponent"
 
 const OtpPage = () => {
+  const userEmail = localStorage.getItem("userEmail")
   return (
     <ContainerWrapper>
-    <Header headerLabel={'Enter OTP'} subHeading= {"Enter the OTP that we have sent to your email address companyadmin@gmail.com."}/>
+    <Header headerLabel={'Enter OTP'} subHeading= {`Enter the OTP that we have sent to your email address ${ userEmail}`}/>
     <OtpComponent />
 
   </ContainerWrapper>
